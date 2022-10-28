a60dabba7c18dddc91a54ec993b0baa0d4bc09f7
OGMaksiu <mateckimaksymilian@gmail.com>
2022-10-28 16:30:30 +0200

    prawie skonczony stos

--- app/src/main/java/com/barstool/zadanie_na_algorytmike_28102022/MainActivity.kt
+++ app/src/main/java/com/barstool/zadanie_na_algorytmike_28102022/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.barstool.zadanie_na_algorytmike_28102022
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import java.util.LinkedList
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val StosDodaj = findViewById<Button>(R.id.btnDodajStos)
+        val StosUsun = findViewById<Button>(R.id.btnUsunStos)
+        val napis = findViewById<TextView>(R.id.napis2)
+        var liczby = LinkedList<Int>();
+        val Wprowadzaniestos = findViewById<EditText>(R.id.stosWpisz)
+        StosDodaj.setOnClickListener {
+            if (liczby.isEmpty()){
+                liczby.addFirst(Wprowadzaniestos.text.toString().toInt())
+                napis.text = liczby.first.toString()
+            }
+            else {
+                liczby.add(Wprowadzaniestos.text.toString().toInt())
+                napis.text = liczby.
+            }
+        }
+        StosUsun.setOnClickListener {
+            liczby.removeFirst()
+            napis.text = liczby.first.toString()
+        }
     }
 }
